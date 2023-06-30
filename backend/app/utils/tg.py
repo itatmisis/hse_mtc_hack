976fac1bb28fe1641e86f2c8bc72edcc0545f4be
algakz06 <77662392+algakz06@users.noreply.github.com>
2023-06-30 13:02:38 +0300

    fixes

--- backend/app/utils/tg.py
+++ backend/app/utils/tg.py
@@ -1,12 +1,13 @@
 import requests
 
+import time
 import asyncio
 
 from app.config import log
 from app.config import settings
 
 
-async def parse_channel(link: str, counter: int = 0) -> bool:
+def parse_channel(link: str, counter: int = 0) -> bool:
     """Parse channel name from link"""
     if counter > 5:
         log.error('Parse channel error')
@@ -24,7 +25,7 @@ async def parse_channel(link: str, counter: int = 0) -> bool:
     if r.status_code == 200:
         pass
     elif r.status_code == 409:
-        await asyncio.sleep(3)
-        await parse_channel(link, counter + 1)
+        time.sleep(4)
+        parse_channel(link, counter + 1)
 
     return True
